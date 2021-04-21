1db9341fc1e7d9185c5e989b9f62dab821c638a1
oyvindsabo <oyvind.sabo@gmail.com>
2021-04-22 01:17:12 +0200

    feat: create getAsciiRepresentation function

---
+++ src/__tests__/getAsciiRepresentation.test.ts
@@ -0,0 +1,16 @@
+import { createGameFromMoves } from '../createGameFromMoves';
+import { getAsciiRepresentation } from '../getAsciiRepresentation';
+
+test('Test that initial game is correctly visualized', () => {
+  expect(getAsciiRepresentation(createGameFromMoves([]))).toStrictEqual({
+    a: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 },
+    b: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 },
+    c: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 },
+    d: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 },
+    e: { 1: 1, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 2 },
+    f: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 },
+    g: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 },
+    h: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 },
+    i: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0 },
+  });
+});
---
+++ src/getAsciiRepresentation.ts
@@ -0,0 +1,132 @@
+import {
+  Game,
+  HorizontalPiecePosition,
+  HorizontalWallPosition,
+  VerticalPiecePosition,
+  VerticalWallPosition,
+} from './types';
+
+export const getAsciiRepresentation = (game: Game) => {
+  let board = '┏━━━';
+  for (let wallIndex = 1; wallIndex <= 10; wallIndex++) {
+    if (game.playerWallCounts[2] >= wallIndex) {
+      board += '║';
+    } else {
+      board += '━';
+    }
+    if (wallIndex === 10) {
+      board += '━━━┓';
+    } else {
+      board += '━━━';
+    }
+  }
+  board += '\n';
+  board += '┃   ';
+  for (let wallIndex = 1; wallIndex <= 10; wallIndex++) {
+    if (game.playerWallCounts[2] >= wallIndex) {
+      board += '║';
+    } else {
+      board += ' ';
+    }
+    if (wallIndex === 10) {
+      board += '   ┃';
+    } else {
+      board += '   ';
+    }
+  }
+  board += '\n';
+  board += '┃   ┌───┬───┬───┬───┬───┬───┬───┬───┬───┐   ┃\n';
+  for (let y: VerticalPiecePosition = 9; y > 0; y--) {
+    let row = '┃   │';
+    Object.keys(game.pieceMatrix)
+      .map((x) => x as HorizontalPiecePosition)
+      .forEach((x) => {
+        if (game.pieceMatrix[x][y] === 0) {
+          row += '   ';
+        } else {
+          row += ` ${game.pieceMatrix[x][y]} `;
+        }
+        if (
+          x !== 'i' &&
+          ((y < 9 &&
+            game.wallMatrix[x as HorizontalWallPosition][
+              y as VerticalWallPosition
+            ].v) ||
+            (y > 1 &&
+              game.wallMatrix[x as HorizontalWallPosition][
+                (y - 1) as VerticalWallPosition
+              ].v))
+        ) {
+          row += '║';
+        } else {
+          row += '│';
+        }
+      });
+    board += `${row}   ┃\n`;
+    if (y > 1) {
+      row = '┃   ├';
+      Object.keys(game.pieceMatrix)
+        .map((x) => x as HorizontalPiecePosition)
+        .forEach((x, index, array) => {
+          if (
+            y < 9 &&
+            ((x !== 'i' &&
+              game.wallMatrix[x][(y - 1) as VerticalWallPosition].h) ||
+              (x !== 'a' &&
+                game.wallMatrix[array[index - 1] as HorizontalWallPosition][
+                  (y - 1) as VerticalWallPosition
+                ].h))
+          ) {
+            row += '═══';
+          } else {
+            row += '───';
+          }
+          if (x !== 'i') {
+            if (
+              y < 9 &&
+              game.wallMatrix[x][(y - 1) as VerticalWallPosition].h
+            ) {
+              row += '═';
+            } else if (
+              y > 1 &&
+              game.wallMatrix[x][(y - 1) as VerticalWallPosition].v
+            ) {
+              row += '║';
+            } else {
+              row += '┼';
+            }
+          }
+        });
+      row += '┤';
+      board += `${row}   ┃\n`;
+    }
+  }
+  board += `┃   └───┴───┴───┴───┴───┴───┴───┴───┴───┘   ┃\n┃   `;
+  for (let wallIndex = 1; wallIndex <= 10; wallIndex++) {
+    if (game.playerWallCounts[1] >= wallIndex) {
+      board += '║';
+    } else {
+      board += ' ';
+    }
+    if (wallIndex === 10) {
+      board += '   ┃';
+    } else {
+      board += '   ';
+    }
+  }
+  board += '\n┗━━━';
+  for (let wallIndex = 1; wallIndex <= 10; wallIndex++) {
+    if (game.playerWallCounts[1] >= wallIndex) {
+      board += '║';
+    } else {
+      board += `━`;
+    }
+    if (wallIndex === 10) {
+      board += `━━━┛`;
+    } else {
+      board += `━━━`;
+    }
+  }
+  board += '\n';
+  return board;
+};
