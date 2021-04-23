1e3edb81fbf18cadd111545cc87da56ffc33de09
oyvindsabo <oyvind.sabo@gmail.com>
2021-04-23 21:02:52 +0200

    fix: I forgot to export isGameOver

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { isValidMove } from './isValidMove';
 export { getAsciiRepresentation } from './getAsciiRepresentation';
 export { makeMove } from './makeMove';
+export { isGameOver } from './isGameOver';
 export { getWinner } from './getWinner';
