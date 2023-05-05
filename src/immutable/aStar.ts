b5a685336887cc57a46c52d158ec51cbfa1a209b
oyvindsabo <oyvind.sabo@gmail.com>
2023-05-05 21:07:52 +0200

    Make tiny performance improvement to getShortestPath by returning earlier in while loop

--- src/immutable/aStar.ts
+++ src/immutable/aStar.ts
@@ -92,22 +92,101 @@ export const aStar = (game: Game, player: Player) => {
   const positionToPreviousPosition: Record<
     PawnPosition,
     PawnPosition | null
-  > = Object.fromEntries(
-    pawnPositions.map((position) => [position, null]),
-  ) as Record<PawnPosition, PawnPosition | null>;
-  // When a node is relaxed it is removed from exploredPositions
+  > = {
+    a1: null,
+    a2: null,
+    a3: null,
+    a4: null,
+    a5: null,
+    a6: null,
+    a7: null,
+    a8: null,
+    a9: null,
+    b1: null,
+    b2: null,
+    b3: null,
+    b4: null,
+    b5: null,
+    b6: null,
+    b7: null,
+    b8: null,
+    b9: null,
+    c1: null,
+    c2: null,
+    c3: null,
+    c4: null,
+    c5: null,
+    c6: null,
+    c7: null,
+    c8: null,
+    c9: null,
+    d1: null,
+    d2: null,
+    d3: null,
+    d4: null,
+    d5: null,
+    d6: null,
+    d7: null,
+    d8: null,
+    d9: null,
+    e1: null,
+    e2: null,
+    e3: null,
+    e4: null,
+    e5: null,
+    e6: null,
+    e7: null,
+    e8: null,
+    e9: null,
+    f1: null,
+    f2: null,
+    f3: null,
+    f4: null,
+    f5: null,
+    f6: null,
+    f7: null,
+    f8: null,
+    f9: null,
+    g1: null,
+    g2: null,
+    g3: null,
+    g4: null,
+    g5: null,
+    g6: null,
+    g7: null,
+    g8: null,
+    g9: null,
+    h1: null,
+    h2: null,
+    h3: null,
+    h4: null,
+    h5: null,
+    h6: null,
+    h7: null,
+    h8: null,
+    h9: null,
+    i1: null,
+    i2: null,
+    i3: null,
+    i4: null,
+    i5: null,
+    i6: null,
+    i7: null,
+    i8: null,
+    i9: null,
+  };
+  // When a node is relaxed it is removed from discoveredPositions
   const discoveredPositions = new Set([startPosition]);
   // This is a subset of discoveredPositions
   const relaxedPositions = new Set<PawnPosition>();
-  let goalPosition: PawnPosition | null = null;
-  while (!goalPosition) {
+  while (true) {
     const mostPromisingPosition = getMostPromisingPosition(
       discoveredPositions,
       player,
     );
     if (!mostPromisingPosition) return null;
     if (hasReachedGoal(mostPromisingPosition, player)) {
-      goalPosition = mostPromisingPosition;
+      return getPath(positionToPreviousPosition, mostPromisingPosition);
     }
     const positionsDiscoveredFromMostPromisingPosition = getValidSurroundingPositions(
       game,
@@ -122,5 +201,4 @@ export const aStar = (game: Game, player: Player) => {
     relaxedPositions.add(mostPromisingPosition);
     discoveredPositions.delete(mostPromisingPosition);
   }
-  return getPath(positionToPreviousPosition, goalPosition);
 };
